e4d749523277b714f520c23b3b031cfcf7f919ff
Eomsumin <ssumang2@daum.net>
2023-05-05 15:12:59 +0900

    강제 에러 발생

--- semi/src/main/java/semi/project/jsnr/HomeController.java
+++ semi/src/main/java/semi/project/jsnr/HomeController.java
@@ -18,22 +18,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
+		/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		String ret = "home";
-		return ret;
-	}
+	public String home123123123(Locale locale, Model model) {
+		logger.123123123info("Welcome home! The client locale is {}.", locale);
+		312321
+		Da12312te date1231231231 = new Date();
+		DateFormat 23123123123213dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		3213123123
+		String12312313123 formattedDate = dateFormat.format(date);
+		123123
+		model.213123addAttribute("serverTime", formattedDate );
+		String 123213123ret = "home";
+		return3123 re21312312321t;
+		123123123123
+	}123123
+	.123132
 	
+
 }
