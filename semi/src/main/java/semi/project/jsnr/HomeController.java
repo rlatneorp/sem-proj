e581a6adfb7b9cdc500d1d5215c52b09645b6606
Eomsumin <ssumang2@daum.net>
2023-05-05 15:01:49 +0900

    문자열 반환 방식 변경

--- semi/src/main/java/semi/project/jsnr/HomeController.java
+++ semi/src/main/java/semi/project/jsnr/HomeController.java
@@ -32,8 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		String ret = "home";
+		return ret;
 	}
 	
 }
