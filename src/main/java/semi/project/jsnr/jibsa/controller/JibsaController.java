e768b569fa74d85f1a0e046fe04c9eeb6087852d
kmjvvv <kimmj_v@naver.com>
2023-05-12 16:39:41 +0900

    jibsaException 추가, 집사 컨트롤러 수정

--- src/main/java/semi/project/jsnr/jibsa/controller/JibsaController.java
+++ src/main/java/semi/project/jsnr/jibsa/controller/JibsaController.java
@@ -1,11 +1,48 @@
 package semi.project.jsnr.jibsa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import semi.project.jsnr.jibsa.model.exception.JibsaException;
+import semi.project.jsnr.jibsa.model.service.JibsaService;
+import semi.project.jsnr.member.model.service.MemberService;
+import semi.project.jsnr.member.model.vo.Member;
 
 @Controller
 public class JibsaController {
+	@Autowired	//의존성 주입
+	private JibsaService jService;
+	private MemberService mService;
+	
+	
+	
+	@RequestMapping("enrollJibsa.js")
+	public String enroll(Member m) {
+		return "enrolljibsa";
+	}
+	
+	@PostMapping("updateJibsa.me")
+	public String editMyInfo(@ModelAttribute Member m, 
+							Model model) {
+		
+		int result = jService.updateJibsa(m);
+		
+		if(result>0) {
+			Member updateJibsa = mService.login(m);
+			model.addAttribute("loginUser",updateJibsa);
+			return " ";
+		} else {
+			throw new JibsaException("정보 수정 실패했습니다.");
+		}
+	}
+	
+	
 	
 	@RequestMapping("jibsaMain.js")
 	public String jibsaMain() {
