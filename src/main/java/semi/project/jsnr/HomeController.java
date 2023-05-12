20be369a0ee60b90850af3a110dc53ca80292f83
janghyunji <ji3605@naver.com>
2023-05-12 11:18:38 +0900

    Member, HomeController 수정
    .do 요청을 HomeController로 이동

--- src/main/java/semi/project/jsnr/HomeController.java
+++ src/main/java/semi/project/jsnr/HomeController.java
@@ -6,16 +6,30 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+
+import semi.project.jsnr.member.model.service.MemberService;
+import semi.project.jsnr.member.model.vo.Member;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private MemberService mService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 		/**
@@ -32,6 +46,53 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		return "redirect:index.jsp";
+	}
+	
+	@RequestMapping("home.do")
+	public String home() {
+		return "redirect:home/home";
+	}
+	
+	@RequestMapping("loginView.do")
+	public String loginView() {
+		return "login/login";
+	}
+	
+	@PostMapping("login.do")
+	public String login(@RequestParam("memberId") String memberId, @RequestParam("memberPwd") String memberPwd, Model model) {
+		Member loginUser = mService.login(memberId);
+		
+		if(loginUser != null) {
+			model.addAttribute("loginUser", loginUser);
+			return "redirect:home.do";
+		} else {
+			return "login/login";
+		}
+		
+	}
+	
+//	// 암호화 할라해ㅔㅆ느데 실패했어요 도와주세요 흑흑 로그인 비번 암호화
+//	@PostMapping("login.do")
+//	public String login(@RequestParam("memberId") String memberId, @RequestParam("memberPwd") String memberPwd, Model model,
+//						@ModelAttribute Member m) {
+//		Member loginUser = mService.login(memberId);
+//		
+//		bcrypt.matches(m.getMemberPwd(), loginUser.getMemberPwd());
+//		
+//		if(bcrypt.matches(m.getMemberPwd(), loginUser.getMemberPwd())) {
+//			model.addAttribute("loginUser", loginUser);
+//			return "redirect:home.do";
+//		} else {
+//			return "login/login";
+//		}
+//		
+//	}
+	
+	@RequestMapping("logout.do")
+	public String logout(SessionStatus status) {
+		status.setComplete();
+		
+		return "redirect:home.do";
 	}
 	
 
--- src/main/java/semi/project/jsnr/member/controller/MemberController.java
+++ src/main/java/semi/project/jsnr/member/controller/MemberController.java
@@ -15,66 +15,15 @@ import org.springframework.web.bind.support.SessionStatus;
 import semi.project.jsnr.member.model.service.MemberService;
 import semi.project.jsnr.member.model.vo.Member;
 
-@SessionAttributes("loginUser")
 @Controller
 public class MemberController {
 	
 	@Autowired
 	private MemberService mService;
 	
-	@Autowired
-	private BCryptPasswordEncoder bcrypt;
-	
 	@GetMapping("myPage.me")
 	public String myPage() {
 		return "myPage";
-	}
-	
-	@RequestMapping("home.do")
-	public String home() {
-		return "redirect:home/home";
-	}
-	
-	@RequestMapping("loginView.do")
-	public String loginView() {
-		return "login/login";
-	}
-	
-	@PostMapping("login.do")
-	public String login(@RequestParam("memberId") String memberId, @RequestParam("memberPwd") String memberPwd, Model model) {
-		Member loginUser = mService.login(memberId);
-		
-		if(loginUser != null) {
-			model.addAttribute("loginUser", loginUser);
-			return "redirect:home.do";
-		} else {
-			return "login/login";
-		}
-		
-	}
-	
-//	// 암호화 할라해ㅔㅆ느데 실패했어요 도와주세요 흑흑
-//	@PostMapping("login.do")
-//	public String login(@RequestParam("memberId") String memberId, @RequestParam("memberPwd") String memberPwd, Model model,
-//						@ModelAttribute Member m) {
-//		Member loginUser = mService.login(memberId);
-//		
-//		bcrypt.matches(m.getMemberPwd(), loginUser.getMemberPwd());
-//		
-//		if(bcrypt.matches(m.getMemberPwd(), loginUser.getMemberPwd())) {
-//			model.addAttribute("loginUser", loginUser);
-//			return "redirect:home.do";
-//		} else {
-//			return "login/login";
-//		}
-//		
-//	}
-	
-	@RequestMapping("logout.do")
-	public String logout(SessionStatus status) {
-		status.setComplete();
-		
-		return "redirect:home.do";
 	}
 	
 	
