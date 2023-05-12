8347f6b44befeb6c314d79e1b185ca3e3361a5d4
kmjvvv <kimmj_v@naver.com>
2023-05-12 17:31:35 +0900

    집사 매퍼

--- src/main/java/semi/project/jsnr/jibsa/model/dao/JibsaDAO.java
+++ src/main/java/semi/project/jsnr/jibsa/model/dao/JibsaDAO.java
@@ -1,8 +1,14 @@
 package semi.project.jsnr.jibsa.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import semi.project.jsnr.member.model.vo.Member;
 
 @Repository
 public class JibsaDAO {
-	
+
+	public int updateJibsa(SqlSessionTemplate sqlSession, Member m) {
+		return sqlSession.update("jibsaMapper.updateJibsa", m);
+	}
 }
