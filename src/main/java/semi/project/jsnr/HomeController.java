bea7d1adeab25a2393314b46cfcfa73a780397b6
janghyunji <ji3605@naver.com>
2023-05-13 17:23:40 +0900

    짜잘한 수정

--- src/main/java/semi/project/jsnr/HomeController.java
+++ src/main/java/semi/project/jsnr/HomeController.java
@@ -50,10 +50,10 @@ public class HomeController {
 		return "redirect:index.jsp";
 	}
 	
-	@RequestMapping("home.do")
-	public String home() {
-		return "redirect:home/home";
-	}
+//	@RequestMapping("home.do")
+//	public String home() {
+//		return "redirect:home/home";
+//	}
 	
 	@RequestMapping("loginView.do")
 	public String loginView() {
